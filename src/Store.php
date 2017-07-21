34f5f066c0a15c539a300d22eaae62d5344aa345
GStewartN <stewart.nathang@gmail.com>
2017-07-21 09:09:52 -0700

    create store class and test class, create test/method getName() - pass

--- src/Store.php
+++ src/Store.php
@@ -1,3 +1,58 @@
 <?php
+    class Store
+    {
+        private $name;
+        private $id;
 
+        function __construct($name, $id = null)
+        {
+            $this->name = $name;
+            $this->id = $id;
+        }
+
+        function getName()
+        {
+            return $this->name;
+        }
+
+        // function setName($new_name)
+        // {
+        //
+        // }
+        //
+        // function getId()
+        // {
+        //
+        // }
+        //
+        // function save()
+        // {
+        //
+        // }
+        //
+        // static function getAll()
+        // {
+        //
+        // }
+        //
+        // static function deleteAll()
+        // {
+        //
+        // }
+        //
+        // static function find()
+        // {
+        //
+        // }
+        //
+        // function update()
+        // {
+        //
+        // }
+        //
+        // function delete()
+        // {
+        //
+        // }
+    }
 ?>
--- tests/StoreTest.php
+++ tests/StoreTest.php
@@ -1,3 +1,101 @@
 <?php
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
 
+    require_once 'src/Store.php';
+
+    $server = 'mysql:host=localhost:8889;dbname=omgshoes_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    class StoreTest extends PHPUnit_Framework_TestCase
+    {
+        function testGetName()
+        {
+            //Arrange
+            $name = "Shoe Stop";
+            $test_store = new Store($name);
+
+            //Act
+            $result = $test_store->getName();
+
+            //Assert
+            $this->assertEquals($name, $result);
+        }
+
+        // function testSetName()
+        // {
+        //     //Arrange
+        //
+        //     //Act
+        //
+        //     //Assert
+        // }
+        //
+        // function testGetId()
+        // {
+        //   //Arrange
+        //
+        //   //Act
+        //
+        //   //Assert
+        // }
+        //
+        // function testSave()
+        // {
+        //   //Arrange
+        //
+        //   //Act
+        //
+        //   //Assert
+        // }
+        //
+        // function testGetAll()
+        // {
+        //   //Arrange
+        //
+        //   //Act
+        //
+        //   //Assert
+        // }
+        //
+        // function testDeleteAll()
+        // {
+        //   //Arrange
+        //
+        //   //Act
+        //
+        //   //Assert
+        // }
+        //
+        // function testFind()
+        // {
+        //   //Arrange
+        //
+        //   //Act
+        //
+        //   //Assert
+        // }
+        //
+        // function testUpdate()
+        // {
+        //   //Arrange
+        //
+        //   //Act
+        //
+        //   //Assert
+        // }
+        //
+        // function testDelete()
+        // {
+        //   //Arrange
+        //
+        //   //Act
+        //
+        //   //Assert
+        // }
+    }
 ?>
